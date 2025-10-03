f78c2ab44bbff8b9959b2365cb034410df294df0
nGincode <fembinurilham@gmail.com>
2025-10-03 23:09:52 +0700

    fix

---
+++ pm2.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "app.easyrubero.com",
+      script: "server/index.js",
+      exec_mode: "fork",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "300M",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
