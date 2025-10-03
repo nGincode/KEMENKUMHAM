ef2db7840f47a63e765ea2931a77272aa37e98a7
nGincode <fembinurilham@gmail.com>
2025-10-03 23:12:52 +0700

    fix

--- pm2.config.js
+++ pm2.config.js
@@ -7,6 +7,7 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
+      version: "1.0.0",
       max_memory_restart: "300M",
       env: {
         NODE_ENV: "production",
