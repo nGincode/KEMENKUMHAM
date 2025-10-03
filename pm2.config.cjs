c68a64cbbe27f82c0b89133252f908fbf2ff5807
nGincode <fembinurilham@gmail.com>
2025-10-04 05:20:23 +0700

    fix

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -9,6 +9,9 @@ module.exports = {
       watch: false,
       version: "1.0.0",
       max_memory_restart: "300M",
+      error_file: "/var/log/pm2/app.easyrubero.com-error.log",
+      out_file: "/var/log/pm2/app.easyrubero.com-out.log",
+      merge_logs: true,
       env: {
         NODE_ENV: "production",
       },
