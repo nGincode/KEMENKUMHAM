93a1e2178a2357575b682923970a8e0f49466ea5
nGincode <fembinurilham@gmail.com>
2026-02-11 21:36:36 +0700

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,36 +1,14 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
-    // Disarankan menggunakan remotePatterns daripada domains (deprecated)
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "app.easyrubero.com",
-      },
-    ],
+    remotePatterns: [{ protocol: "https", hostname: "app.easyrubero.com" }],
   },
-
   env: {
     HOSTNAME: process.env.HOSTNAME,
     PORT: process.env.PORT,
     URL: process.env.URL,
-  },
-
-  experimental: {
-    optimizePackageImports: ["lucide-react", "some-ui-library"],
-  },
-  webpack: (config, { isServer }) => {
-    config.plugins.push(
-      new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery",
-        "window.jQuery": "jquery",
-      }),
-    );
-    return config;
   },
 };
 
